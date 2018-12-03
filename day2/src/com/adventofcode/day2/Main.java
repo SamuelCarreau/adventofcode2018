333f3676b5e6cd2f3f88fd0d46c1710cfa1d7725
samuel carreau <samuelcarreau@gmail.com>
2018-12-02 22:36:01 -0500

    add day2

---
+++ day2/src/com/adventofcode/day2/Main.java
@@ -0,0 +1,41 @@
+package com.adventofcode.day2;
+
+import com.adventofcode.day2.utilities.FileHelper;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filename = "src\\boxIdInput.txt";
+
+        int doubleCounter = 0;
+        int tripleCounter = 0;
+
+        try {
+            List<String> boxIds = FileHelper.readTextFile(filename);
+
+
+            for (String boxId : boxIds) {
+                BoxScore boxScore = new BoxScore(boxId);
+                doubleCounter += boxScore.haveDoublet();
+                tripleCounter += boxScore.haveTriplet();
+            }
+        }catch (Exception exc){
+            System.out.println(exc.getMessage());
+        }
+
+        System.out.println("The value of "+doubleCounter+ " * "+tripleCounter+ " = "+doubleCounter*tripleCounter);
+
+
+
+
+
+
+    }
+
+
+
+}
